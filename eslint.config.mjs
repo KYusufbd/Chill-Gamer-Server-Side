02ab752c890d363ad91e764f15befe860446b59a
Kazi Yusuf <yfaka001@gmail.com>
2025-02-20 16:15:54 +0600

    Connected to mongodb successfully alhamdulillah.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 
 
-/** @type {import('eslint').Linter.Config["Node"]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    "env": {
+      "browser": true,
+      "commonjs": true,
+      "es2021": true,
+      "node" : true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+      "ecmaVersion": 12
+    },
+    "rules": {}
+  },
 ];
